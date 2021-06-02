49e1250b7ccb24e24ab62405fdb24332ff65b137
markh1089 <markhempel89@gmail.com>
2021-06-02 13:03:02 +0100

    added background

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './Components/Navigation';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <h2>Portfolio</h2>
+      <Header />
 
     </div>
   );
---
+++ portfolio/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className='main-text'>
+                Mark Hempel
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
