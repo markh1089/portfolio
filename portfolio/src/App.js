2692c60eb3e8c5fd40621651e2b7c70a47c07d08
markh1089 <markhempel89@gmail.com>
2021-06-02 12:21:04 +0100

    set up bootstrap and removed files we don't need from boiler plate

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Portfolio</h2>
+
+    </div>
+  );
+}
+
+export default App;
