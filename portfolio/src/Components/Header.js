01e40359738a42696aa5387e810bb23ef0538d5e
markh1089 <markhempel89@gmail.com>
2021-06-14 20:27:09 +0100

    added img

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Typed from "react-typed";
-import { Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import MyImg from "../mark.jpg"
 
 
 
@@ -9,6 +10,7 @@ function Header() {
     <div className="header">
       <div className="main-text">
         <h1>Mark Hempel</h1>
+        <img className="me" src={MyImg} />
         <Typed 
         className='typed-text'
         strings={['html', 'css','Javascript','React','Web Developer']}
